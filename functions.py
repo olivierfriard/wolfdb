4e8c06919470cc33755a3948f3b049b9fc21d6f7
Olivier Friard <olivier.friard@unito.it>
2021-11-19 15:30:03 +0100

    some changes

--- functions.py
+++ functions.py
@@ -88,3 +88,102 @@ def get_regions(provinces):
 
     return " ".join(list(set(transect_region)))
 
+
+def leaflet_point(point_lonlat: list, scat_id: str) -> str:
+
+    lon, lat = point_lonlat
+
+    map = f"""
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+
+    <script>
+	var map = L.map('map').setView([{lat}, {lon}], 13);
+
+L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}}).addTo(map);
+
+
+var blueIcon = L.icon({{ iconUrl: '/static/marker-icon-blue.png', iconAnchor: [7, 20] }});
+
+var marker = L.marker([{lat},  {lon}], {{icon: blueIcon}}).addTo(map);
+marker.bindPopup("{scat_id}");
+
+/*
+function onMapClick(e) {{
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
+	}}
+
+	map.on('click', onMapClick);
+*/
+var popup = L.popup();
+	</script>
+    """
+
+    return map
+
+
+
+
+def leaflet_line(points_lonlat: list) -> str:
+
+    # UTM coord conversion
+    '''
+    print(points[0:10])
+    points_latlon = [list(utm.to_latlon(x, y, 32, "N")) for x, y in points]
+    print(points_latlon[0:10])
+    '''
+
+    lon_org, lat_orig = points_lonlat[0]
+
+    points_latlong = [[lat, lon] for lon, lat in points_lonlat]
+
+    map = f"""
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+
+    <script>
+	var map = L.map('map').setView([{lat_orig}, {lon_org}], 13);
+
+L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}}).addTo(map);
+
+
+
+var polylinePoints = {points_latlong};
+
+var firstpolyline = L.polyline(polylinePoints, {{
+    color: 'red',
+    opacity: 0.75,
+    smoothFactor: 1
+
+    }}).addTo(map);
+
+function onMapClick(e) {{
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
+	}}
+
+	map.on('click', onMapClick);
+var popup = L.popup();
+	</script>
+    """
+
+    return map
