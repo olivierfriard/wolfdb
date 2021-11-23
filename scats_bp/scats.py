ac819a68e58664026f6e7fc3306be8d750376d5a
Olivier Friard <olivier.friard@unito.it>
2021-11-23 09:35:34 +0100

    some changes

--- scats_bp/scats.py
+++ scats_bp/scats.py
@@ -103,24 +103,31 @@ def view_scat(scat_id):
     results["lonlat_str"] = f"{lon_lat[0]}, {lon_lat[1]}"
 
     # transect
-    transect_id = "_".join(results["path_id"].split("_")[:-1])
+    if results["path_id"]:
+        transect_id = "_".join(results["path_id"].split("_")[:-1])
 
-    cursor.execute("SELECT ST_AsGeoJSON(points) AS transect_geojson FROM transects WHERE transect_id = %s",
-                   [transect_id])
-    transect = cursor.fetchone()
+        cursor.execute("SELECT ST_AsGeoJSON(points) AS transect_geojson FROM transects WHERE transect_id = %s",
+                    [transect_id])
+        transect = cursor.fetchone()
 
-    transect_geojson = json.loads(transect["transect_geojson"])
+        transect_geojson = json.loads(transect["transect_geojson"])
 
 
-    transect_feature = {
-            "type": "Feature",
-            "geometry": dict(transect_geojson),
-            "properties": {
-                "popupContent": f"Transect ID: {transect_id}"
-            },
-            "id": 1
-        }
-    transect_features = [transect_feature]
+        transect_feature = {
+                "type": "Feature",
+                "geometry": dict(transect_geojson),
+                "properties": {
+                    "popupContent": f"Transect ID: {transect_id}"
+                },
+                "id": 1
+            }
+        transect_features = [transect_feature]
+
+    else:
+        # opportunistic
+        transect_features = []
+
+
 
     return render_template("view_scat.html",
                            results=results,
@@ -650,7 +657,8 @@ def extract_data_from_xlsx(filename):
                     'deposition', 'matrix', 'collected_scat', 'scalp_category',
                     'genetic_sample',
                     'coord_east', 'coord_north', 'coord_zone',
-                    'operator', 'institution']:
+                    'operator', 'institution', 'notes']:
+
         if column not in list(scats_df.columns):
             return True, fn.alert_danger(f"Column {column} is missing"), {}, {}, {}
 
@@ -667,19 +675,25 @@ def extract_data_from_xlsx(filename):
             year = int(data['scat_id'][1:2+1]) + 2000
             month = int(data['scat_id'][3:4+1])
             day = int(data['scat_id'][5:6+1])
-            date = f"{year}-{month}-{day}"
+            date = f"{year}-{month:02}-{day:02}"
         except Exception:
-            return True, fn.alert_danger(f"The scat ID is not valid at row {index + 1}: {data['scat_id']}"), {}, {}, {}
+            return True, fn.alert_danger(f"The scat ID is not valid at row {index + 2}: {data['scat_id']}"), {}, {}, {}
+
 
         # check date
-        print(f"{data['date']=}")
+        print(f"date from scat ID: {date}")
+        #print(f"{data['date']=}")
         print(f"{str(data['date'])=}")
         try:
             date_from_file = str(data["date"]).split(" ")[0].strip()
         except Exception:
             date_from_file = ""
+
+        print(f"date from file: {date_from_file}")
+
+
         if date != date_from_file:
-            return True, fn.alert_danger(f"Check the scat ID and the date at row {index + 1}: {data['scat_id']}  {date_from_file}"), {}, {}, {}
+            return True, fn.alert_danger(f"Check the scat ID and the date at row {index + 2}: {data['scat_id']}  {date_from_file}"), {}, {}, {}
 
         data["date"] = date_from_file
 
@@ -695,37 +709,55 @@ def extract_data_from_xlsx(filename):
         try:
             coord_latlon = utm.to_latlon(int(data["coord_east"]), int(data["coord_north"]), int(data["coord_zone"].replace("N", "")), "N")
         except Exception:
-            return True, fn.alert_danger(f'Check the UTM coordinates at row {index + 1}: {data["coord_east"]} {data["coord_north"]} {data["coord_zone"]}'), {}, {}, {}
+            return True, fn.alert_danger(f'Check the UTM coordinates at row {index + 2}: {data["coord_east"]} {data["coord_north"]} {data["coord_zone"]}'), {}, {}, {}
         data["coord_latlon"] = f"SRID=4326;POINT({coord_latlon[1]} {coord_latlon[0]})"
 
         # sampling_type
-        if data["sampling_type"].upper().strip() not in ["OPPORTUNISTIC", "SYSTEMATIC"]:
-            return True, fn.alert_danger(f'Sampling type must be <b>Opportunistic</b> or <b>Systematic</b> at row {index + 1}'), {}, {}, {}
+        data["sampling_type"] = data["sampling_type"].capitalize().strip()
+        if data["sampling_type"] not in ["Opportunistic", "Systematic"]:
+            return True, fn.alert_danger(f'Sampling type must be <b>Opportunistic</b> or <b>Systematic</b> at row {index + 2}'), {}, {}, {}
+
+        # no path ID if scat is opportunistc
+        if data["sampling_type"] == "Opportunistic":
+            data["path_id"] = ""
 
         # deposition
-        if data["deposition"].upper().strip() == "FRESCA":
+        data["deposition"] = data["deposition"].capitalize().strip()
+        if data["deposition"] == "Fresca":
             data["deposition"] = "Fresh"
-        if data["deposition"].upper().strip() == "VECCHIA":
+        if data["deposition"] == "Vecchia":
             data["deposition"] = "Old"
-        if data["deposition"].upper().strip() not in ["FRESH", "OLD"]:
-            return True, fn.alert_danger(f'The deposition must be <b>fresh</b> or <b>old</b> at row {index + 1}'), {}, {}, {}
-
-        # collected_scat
-        if data["collected_scat"].upper().strip() == "SI":
-            data["collected_scat"] = "Yes"
-        if data["collected_scat"].upper().strip() == "NO":
-            data["collected_scat"] = "No"
-        if data["collected_scat"].upper().strip() not in ["YES", "NO"]:
-            return True, fn.alert_danger(f'The collected_scat must be <b>Yes</b> or <b>No</b> at row {index + 1}'), {}, {}, {}
+        if data["deposition"] not in ["Fresh", "Old", ""]:
+            return True, fn.alert_danger(f'The deposition value must be <b>Fresh</b>, <b>Old</b> or empty at row {index + 2}'), {}, {}, {}
 
         # matrix
-        if data["matrix"].upper().strip() == "SI":
+        data["matrix"] = data["matrix"].capitalize().strip()
+        if data["matrix"] in ["Si", "Sì"]:
             data["matrix"] = "Yes"
-        if data["matrix"].upper().strip() == "NO":
+        if data["matrix"] == "No":
             data["matrix"] = "No"
+        if data["matrix"] not in ["Yes", "No", ""]:
+            return True, fn.alert_danger(f'The matrix value must be <b>Yes</b> or <b>No</b> or empty at row {index + 2}'), {}, {}, {}
 
-        if data["matrix"].upper().strip() not in ["YES", "NO"]:
-            return True, fn.alert_danger(f'The matrix must be <b>Yes</b> or <b>No</b> at row {index + 1}'), {}, {}, {}
+        # collected_scat
+        data["collected_scat"] = data["collected_scat"].capitalize().strip()
+        if data["collected_scat"] in ["Si", "Sì"]:
+            data["collected_scat"] = "Yes"
+        if data["collected_scat"] == "No":
+            data["collected_scat"] = "No"
+        if data["collected_scat"] not in ["Yes", "No", ""]:
+            return True, fn.alert_danger(f'The collected_scat value must be <b>Yes</b> or <b>No</b> or empty at row {index + 2}'), {}, {}, {}
+
+        # genetic_sample
+        data["genetic_sample"] = data["genetic_sample"].capitalize().strip()
+        if data["genetic_sample"] in ["Si", "Sì"]:
+            data["genetic_sample"] = "Yes"
+        if data["genetic_sample"] == "No":
+            data["genetic_sample"] = "No"
+        if data["genetic_sample"] not in ["Yes", "No", ""]:
+            return True, fn.alert_danger(f'The genetic_sample value must be <b>Yes</b> or <b>No</b> or empty at row {index + 2}'), {}, {}, {}
+
+
 
         all_data[index] = dict(data)
 
@@ -846,24 +878,27 @@ def confirm_load_xlsx(filename, mode):
             "                matrix = %(matrix)s, "
             "                collected_scat = %(collected_scat)s, "
             "                scalp_category = %(scalp_category)s, "
+            "                genetic_sample = %(genetic_sample)s, "
             "                coord_east = %(coord_east)s, "
             "                coord_north = %(coord_north)s, "
             "                coord_zone = %(coord_zone)s, "
             "                observer = %(operator)s, "
             "                institution = %(institution)s, "
-            "                geo = %(geo)s "
+            "                geo = %(geo)s, "
+            "                notes = %(notes)s "
             "WHERE scat_id = %(scat_id)s;"
 
             "INSERT INTO scats (scat_id, date, wa_code, genotype_id, sampling_season, sampling_type, path_id, snowtrack_id, "
             "location, municipality, province, region, "
-            "deposition, matrix, collected_scat, scalp_category, "
+            "deposition, matrix, collected_scat, scalp_category, genetic_sample, "
             "coord_east, coord_north, coord_zone, "
             "observer, institution,"
-            "geo) "
+            "geo, notes) "
             "SELECT %(scat_id)s, %(date)s, %(wa_code)s, %(genotype_id)s, "
             " %(sampling_season)s, %(sampling_type)s, %(path_id)s, %(snowtrack_id)s, "
-            "%(location)s, %(municipality)s, %(province)s, %(region)s, %(deposition)s, %(matrix)s, %(collected_scat)s, %(scalp_category)s, "
-            " %(coord_east)s, %(coord_north)s, %(coord_zone)s, %(operator)s, %(institution)s, %(geo)s "
+            "%(location)s, %(municipality)s, %(province)s, %(region)s, "
+            "%(deposition)s, %(matrix)s, %(collected_scat)s, %(scalp_category)s, %(genetic_sample)s,"
+            " %(coord_east)s, %(coord_north)s, %(coord_zone)s, %(operator)s, %(institution)s, %(geo)s, %(notes)s "
             "WHERE NOT EXISTS (SELECT 1 FROM scats WHERE scat_id = %(scat_id)s)"
             )
     count_added = 0
@@ -884,16 +919,18 @@ def confirm_load_xlsx(filename, mode):
                         "wa_code": data["wa_code"].strip(),
                         "genotype_id": data["genotype_id"].strip(),
                         "sampling_season": fn.sampling_season(data["date"]),
-                        "sampling_type": data["sampling_type"].strip(),
+                        "sampling_type": data["sampling_type"],
                         "path_id": data["path_id"],
                         "snowtrack_id": data["snowtrack_id"].strip(),
                         "location": data["location"].strip(), "municipality": data["municipality"].strip(),
                         "province": data["province"].strip().upper(), "region": data["region"],
-                        "deposition": data["deposition"], "matrix": data["matrix"].strip(),
-                        "collected_scat": data["collected_scat"].strip(), "scalp_category": data["scalp_category"].strip(),
+                        "deposition": data["deposition"], "matrix": data["matrix"],
+                        "collected_scat": data["collected_scat"], "scalp_category": data["scalp_category"].strip(),
+                        "genetic_sample": data["genetic_sample"],
                         "coord_east": data["coord_east"], "coord_north": data["coord_north"], "coord_zone": data["coord_zone"].strip(),
-                        "operator": data["operator"].strip(),   "institution": data["institution"].strip(),
+                        "operator": data["operator"].strip(), "institution": data["institution"].strip(),
                         "geo": data["coord_latlon"],
+                        "notes": data["notes"].strip()
                         }
                         )
     connection.commit()
