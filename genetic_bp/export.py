57ad2bd767080f8cf4b4668759a7e496d2bd4ccd
Olivier Friard <olivier.friard@unito.it>
2022-01-04 10:14:26 +0100

    some work

---
+++ genetic_bp/export.py
@@ -0,0 +1,99 @@
+"""
+export results in XLSX format
+"""
+
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill, Font
+from tempfile import NamedTemporaryFile
+
+def export_wa_genetic_samples(loci_list, wa_scats, loci_values):
+
+    wb = Workbook()
+
+    ws1 = wb.active
+    ws1.title = f"WA genetic samples"
+
+    header = ["WA code", "Sample ID", "Date", "Municipality", "Coordinates WGS84 UTM zone 32N", "mtDNA result",
+                "Genotype ID", "Temporary ID", "Sex"]
+    for locus in loci_list:
+        header.extend([f"{locus} a", f"Notes for {locus} a"])
+        if loci_list[locus] == 2:
+            header.extend([f"{locus} b", f"Notes for {locus} b"])
+
+    ws1.append(header)
+
+
+    for row in wa_scats:
+        out = []
+        out.append(row["wa_code"])
+        out.append(row["sample_id"] if row["sample_id"] is not None else "")
+        out.append(row["date"] if row["date"] is not None else "")
+        out.append(row["municipality"] if row["municipality"] is not None else "")
+        out.append(f'{row["coord_east"]}, {row["coord_north"]}')
+        out.append(row["mtdna"] if row["mtdna"] is not None else "")
+        out.append(row["genotype_id"] if row["genotype_id"] is not None else "")
+        out.append(row["tmp_id"] if row["tmp_id"] is not None else "")
+        out.append(row["sex_id"] if row["sex_id"] is not None else "")
+
+        for locus in loci_list:
+            out.extend([loci_values[row['wa_code']][locus]['a']['value'],
+                        loci_values[row['wa_code']][locus]['a']['notes'],
+                        loci_values[row['wa_code']][locus]['b']['value'],
+                        loci_values[row['wa_code']][locus]['b']['notes'],
+                       ]
+                      )
+
+        ws1.append(out)
+
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        stream = tmp.read()
+
+        return stream
+
+
+
+def export_wa_analysis_group(loci_list, data, loci_values):
+
+    wb = Workbook()
+
+    ws1 = wb.active
+    ws1.title = f"Genotype matches"
+
+    header = ["Genotype ID", "Temporary ID", "Sex", "Status", "Pack", "Number of recaptures"]
+    for locus in loci_list:
+        header.extend([f"{locus} a", f"Notes for {locus} a"])
+        if loci_list[locus] == 2:
+            header.extend([f"{locus} b", f"Notes for {locus} b"])
+
+    ws1.append(header)
+
+    for genotype_id in data:
+        out = []
+        
+        out.append(genotype_id)
+        out.append(data[genotype_id]["tmp_id"])
+        out.append(data[genotype_id]["sex"])
+        out.append(data[genotype_id]["position"])
+        out.append(data[genotype_id]["pack"])
+        out.append(data[genotype_id]["n_recap"])
+
+        for locus in loci_list:
+            out.extend([loci_values[genotype_id][locus]['a']['value'],
+                        loci_values[genotype_id][locus]['a']['notes'],
+                        loci_values[genotype_id][locus]['b']['value'],
+                        loci_values[genotype_id][locus]['b']['notes'],
+                       ]
+                      )
+
+        ws1.append(out)
+
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        stream = tmp.read()
+
+        return stream
+    
