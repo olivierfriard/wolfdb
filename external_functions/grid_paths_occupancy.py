108180423a00f59b49804fe96f721b371b360863
Olivier Friard <olivier.friard@unito.it>
2022-04-07 15:42:53 +0200

    some changes

---
+++ external_functions/grid_paths_occupancy.py
@@ -0,0 +1,71 @@
+"""
+read shapefile with fiona module
+
+"""
+
+
+import sys
+import fiona
+import psycopg2
+import psycopg2.extras
+import functions as fn
+
+connection = fn.get_connection()
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+shapes = fiona.open(sys.argv[1])
+
+
+data = {}
+
+for shape in shapes:
+
+    id = shape["id"]
+    print(f"cell ID: {id}", file=sys.stderr)
+
+    data[id] = {}
+
+    # print(shape["properties"])
+    # print(shape["geometry"]["type"])
+    # print(f"{shape['geometry']['coordinates']=}")
+
+    coord_list = shape["geometry"]["coordinates"][0]
+    coord_str = ", ".join([f"{round(x[0])} {round(x[1])}" for x in coord_list])
+
+    sql = f"SELECT transect_id FROM transects WHERE ST_INTERSECTS(ST_GeomFromText('POLYGON(({coord_str}))', 32632), multilines); "
+
+    cursor.execute(sql)
+    rows2 = cursor.fetchall()
+
+    for row2 in rows2:
+        print(f'transect ID: {row2["transect_id"]}\t', file=sys.stderr)
+
+        cursor.execute("SELECT path_id, date::date FROM paths WHERE transect_id = %s", [row2["transect_id"]])
+        rows3 = cursor.fetchall()
+
+        for row3 in rows3:
+            if row3["date"] not in data[id]:
+                data[id][f"{row3['date']:%Y-%m-%d}"] = 0
+
+            cursor.execute(
+                f"SELECT count(scat_id) AS count FROM scats WHERE path_id = %s AND ST_CONTAINS(ST_GeomFromText('POLYGON(({coord_str}))', 32632), geometry_utm)",
+                [row3["path_id"]],
+            )
+            row4 = cursor.fetchone()
+            data[id][f"{row3['date']:%Y-%m-%d}"] += row4["count"]
+            print(f"{row3['path_id']}\tnumber of scats: {row4['count']}", file=sys.stderr)
+
+    print("=" * 50, file=sys.stderr)
+
+# print(data)
+tot_scats_nb = 0
+
+sep = "\t"
+for id in data:
+    tot_scats_nb += sum([data[id][date] for date in data[id]])
+    print(f"{id}{sep}{ f'{sep}'.join([str(data[id][date]) for date in sorted(data[id].keys())])}")
+
+    # print(f"{id}{sep}{ f'{sep}'.join([str(int(data[id][date] > 0)) for date in sorted(data[id].keys())])}")
+
+
+print(f"{tot_scats_nb=}", file=sys.stderr)
---
+++ external_functions/paths_completeness.py
@@ -0,0 +1,127 @@
+"""
+create shapefile (using fiona)
+with all paths based on the completeness
+"""
+
+import sys
+import os
+import json
+import fiona
+import shutil
+
+import psycopg2
+import psycopg2.extras
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import functions as fn
+
+
+def create_shapefile(dir_path: str, log_file: str):
+
+    log = open(log_file, "w")
+
+    connection = fn.get_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cursor.execute("SELECT * FROM paths")
+    paths = cursor.fetchall()
+
+    schema = {
+        "geometry": "LineString",
+        "properties": [
+            ("province", "str"),
+            ("transect_id", "str"),
+            ("path_id", "str"),
+            ("completeness", "int"),
+            ("date", "str"),
+            ("month", "str"),
+            ("observer", "str"),
+            ("institution", "str"),
+            ("category", "str"),
+        ],
+    }
+
+    pointShp = fiona.open(dir_path, mode="w", driver="ESRI Shapefile", schema=schema, crs="EPSG:32632")
+
+    for path in paths:
+
+        print(file=log)
+        print(path, file=log)
+        if path["transect_id"] is None or path["transect_id"] == "":
+            print(f"No transect in path", file=log)
+            continue
+
+        cursor.execute(
+            (
+                "SELECT *, "
+                "ST_AsGeoJSON(multilines) AS transect_geojson, "
+                "ROUND(ST_Length(multilines)) AS transect_length "
+                "FROM transects "
+                "WHERE transect_id = %s"
+            ),
+            [path["transect_id"]],
+        )
+
+        transect = cursor.fetchone()
+        # print(transect)
+
+        if transect is None:
+            print(f"path['transect_id'] NOT FOUND", file=log)
+            continue
+
+        if path["completeness"]:
+
+            print(transect["transect_length"], file=log)
+
+            transect_geojson = json.loads(transect["transect_geojson"])
+            if len(transect_geojson["coordinates"]) == 1:
+                # print(transect_geojson["coordinates"])
+
+                tot_dist = 0
+                new_list = []
+                for idx, point in enumerate(transect_geojson["coordinates"][0]):
+                    if idx == 0:  # skip first point
+                        continue
+                    d = (
+                        (point[0] - transect_geojson["coordinates"][0][idx - 1][0]) ** 2
+                        + (point[1] - transect_geojson["coordinates"][0][idx - 1][1]) ** 2
+                    ) ** 0.5
+                    tot_dist += d
+
+                    # print(point, d, tot_dist, transect["transect_length"], file=log)
+
+                    new_list.append(point)
+                    if round((tot_dist / transect["transect_length"]) * 100) >= path["completeness"]:
+                        break
+
+                if round((tot_dist / transect["transect_length"]) * 100) >= path["completeness"]:
+                    print(f'{path["completeness"]} COMPLETE OK  {tot_dist / transect["transect_length"]}', file=log)
+
+                    rowDict = {
+                        "geometry": {"type": "LineString", "coordinates": new_list},
+                        "properties": {
+                            "province": transect["province"],
+                            "transect_id": path["transect_id"],
+                            "path_id": path["path_id"],
+                            "completeness": path["completeness"],
+                            "date": path["date"].isoformat(),
+                            "month": path["date"].isoformat().split("-")[1],
+                            "observer": path["observer"],
+                            "institution": path["institution"],
+                            "category": path["category"],
+                        },
+                    }
+                    pointShp.write(rowDict)
+
+                else:
+                    print(f'{path["completeness"]} NOT COMPLETE {tot_dist / transect["transect_length"]}', file=log)
+
+    log.close()
+
+    # make a ZIP archive
+    zip_file_name = shutil.make_archive(dir_path, "zip", dir_path)
+
+    return zip_file_name
+
+
+create_shapefile(sys.argv[1], sys.argv[2])
