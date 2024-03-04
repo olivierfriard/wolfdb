90036c05512734bc513f940da627db23ea25d8f5
Olivier Friard <olivier.friard@unito.it>
2024-03-04 15:09:15 +0100

    some work

--- update_redis_with_wa_loci_values.py
+++ update_redis_with_wa_loci_values.py
@@ -5,8 +5,8 @@ This script is required by wolfdb.py
 
 """
 
+import sys
 from sqlalchemy import text
-
 import functions as fn
 import json
 import redis
@@ -14,16 +14,15 @@ import redis
 from config import config
 
 params = config()
+if not params:
+    print("Parameters not found")
+    sys.exit()
 
 # dev version use db 0
 rdis = redis.Redis(db=(0 if params["database"] == "wolf" else 1))
 
-
 # loci list
 loci_list: dict = fn.get_loci_list()
-
-print(f"{loci_list=}")
-
 
 with fn.conn_alchemy().connect() as con:
     sql = text("SELECT wa_code FROM wa_scat_dw_mat WHERE UPPER(mtdna) not like '%POOR DNA%' ")
